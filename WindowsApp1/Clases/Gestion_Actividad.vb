de21da553809962300c2b6e968403351a807266f
Jarol Rojas Reyes <jr.rojas0327@gmail.com>
2025-04-07 14:03:31 +0200

    commit vacio

--- WindowsApp1/Clases/Gestion_Actividad.vb
+++ WindowsApp1/Clases/Gestion_Actividad.vb
@@ -1,11 +1,1 @@
-﻿Imports System.Data.SqlClient
-
-Public Class Gestion_Actividad
-    Private servidor = ""
-    Private cadenaConexion = $"Data Source = {servidor}; Initial Catalog = Estado; Integrated Security = SSPI; MultiplerActiveResultSets = true"
-    Dim conexion As New SqlConnection(cadenaConexion)
-
-
-
-
-End Class
+﻿
